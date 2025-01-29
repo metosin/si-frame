bb509e17992a8906fb6c4a2f82760955afc549cf
Vincent Cantin <vincent@404.taipei>
2025-01-29 21:48:40 +0200

    Small simplification: use-reactive-node can use `react/useMemo` instead of `uix/use-memo`.

--- src/re_frame/uix.cljs
+++ src/re_frame/uix.cljs
@@ -5,24 +5,24 @@
             [uix.core :as uix]))
 
 (defn use-reactive-node [reactive-node]
-  (let [[subscribe get-snapshot] (uix/use-memo (fn []
-                                                 ;;(prn "in use-memo")
-                                                 (let [subscribe (fn [ping-react-that-something-might-have-changed]
-                                                                   (let [signal-watcher (reify
-                                                                                          sr/ISignalWatcher
-                                                                                          (notify-signal-watcher [this is-for-sure signal-source]
-                                                                                            (ping-react-that-something-might-have-changed)))]
-                                                                     ;;(prn "subscribe")
-                                                                     (sr/add-signal-watcher reactive-node signal-watcher)
-                                                                     (fn []
-                                                                       ;;(prn "unsubscribe")
-                                                                       (sr/remove-signal-watcher reactive-node signal-watcher))))
-                                                       get-snapshot (fn []
-                                                                      ;;(prn "in get-snapshot")
-                                                                      ;; https://stackoverflow.com/questions/76474940/why-does-the-usesyncexternalstore-example-call-getsnapshot-6-times-on-store
-                                                                      @reactive-node)]
-                                                   [subscribe get-snapshot]))
-                                               [reactive-node])]
+  (let [[subscribe get-snapshot] (react/useMemo (fn []
+                                                  ;;(prn "in use-memo")
+                                                  (let [subscribe (fn [ping-react-that-something-might-have-changed]
+                                                                    (let [signal-watcher (reify
+                                                                                           sr/ISignalWatcher
+                                                                                           (notify-signal-watcher [this is-for-sure signal-source]
+                                                                                             (ping-react-that-something-might-have-changed)))]
+                                                                      ;;(prn "subscribe")
+                                                                      (sr/add-signal-watcher reactive-node signal-watcher)
+                                                                      (fn []
+                                                                        ;;(prn "unsubscribe")
+                                                                        (sr/remove-signal-watcher reactive-node signal-watcher))))
+                                                        get-snapshot (fn []
+                                                                       ;;(prn "in get-snapshot")
+                                                                       ;; https://stackoverflow.com/questions/76474940/why-does-the-usesyncexternalstore-example-call-getsnapshot-6-times-on-store
+                                                                       @reactive-node)]
+                                                    [subscribe get-snapshot]))
+                                               #js [reactive-node])]
     (react/useSyncExternalStore subscribe get-snapshot)))
 
 (defn use-subscribe [query-v]
